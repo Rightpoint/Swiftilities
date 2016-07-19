44170e2f0a0d80aada03abf8a9213adb0ca5787a
Derek Ostrander <djostran@gmail.com>
2016-07-19 08:38:24 -0700

    Bundle fix

--- Pod/Classes/Logo/UIView+Logo.swift
+++ Pod/Classes/Logo/UIView+Logo.swift
@@ -10,7 +10,7 @@ import UIKit
 
 public extension UIView {
     public static func rz_logo(contentMode: UIViewContentMode = .Center, insets: UIEdgeInsets = UIEdgeInsetsZero) -> UIView {
-        let image = UIImage(named: "logo-built-by-RZ")!.imageWithRenderingMode(.AlwaysTemplate).resizableImageWithCapInsets(insets)
+        let image = UIImage(named: "logo-built-by-RZ", inBundle: NSBundle(forClass: Swiftilities.UIView.self), compatibleWithTraitCollection: nil)!.imageWithRenderingMode(.AlwaysTemplate).resizableImageWithCapInsets(insets)
         let logo = UIImageView(image: image)
         logo.contentMode = contentMode
         return logo
