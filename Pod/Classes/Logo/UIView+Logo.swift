8afbfa1aa6cad216b14dcd1f603de230380b204a
Derek Ostrander <djostran@gmail.com>
2016-07-19 09:06:27 -0700

    RZLogo

--- Pod/Classes/Logo/UIView+Logo.swift
+++ Pod/Classes/Logo/UIView+Logo.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-extension UIView {
-    static func rz_logo(contentMode: UIViewContentMode = .Center, insets: UIEdgeInsets = UIEdgeInsetsZero) -> UIView {
-        let image = UIImage(named: "logo-built-by-RZ")!.resizableImageWithCapInsets(insets)
+public extension UIView {
+    private class RZBundle {} // to grab the image url
+    public static func rz_logo() -> UIImageView {
+        guard let url = NSBundle(forClass: RZBundle.self).URLForResource("logo-built-by-RZ", withExtension: "png"),
+            let path = url.path,
+            let image = UIImage(contentsOfFile: path) else { fatalError("Can't find the rz logo in the bundle.") }
         let logo = UIImageView(image: image)
-        logo.contentMode = contentMode
+        logo.contentMode = .ScaleAspectFit
         return logo
     }
 }
