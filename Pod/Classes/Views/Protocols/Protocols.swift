719361ca81ecde6a4584c08a61eb4e65f4b64eb1
nevillco <connor.neville@raizlabs.com>
2016-08-16 09:45:27 -0400

    Removed extra whitespace/newline

--- Pod/Classes/Views/Protocols/Protocols.swift
+++ Pod/Classes/Views/Protocols/Protocols.swift
@@ -19,18 +19,16 @@ protocol PlaceholderConfigurable {
 }
 
 public protocol TextViewAnimationDelegate {
-
     /// Whether or not the given UITextView should animate its height changes.
     /// - Parameter textView: The UITextView that should or should not have the changes to its height animated.
     /// - Returns: Whether or not changes to the height of the UITextView will be animated.
     func shouldAnimateHeightChange(textView: UITextView) -> Bool
-    
-    
+
     /// Gets the container view that will call `setNeedsLayout` in order to animate the given UITextView. Should be a parent of `textView`, but if not, the call will have no effect. Will also have no effect if `shouldAnimateHeightChange` returns false.
     /// - Parameter textView: The UITextView whose height is to be changed.
     /// - Returns: The UIView responsible for animating layout changes by laying out its subviews.
     func containerToLayout(forTextView textView: UITextView) -> UIView?
-    
+
     /// Gets the animation duration for when `textView` changes height. Value is not used if `shouldAnimateHeightChange` returns false.
     /// - Parameter textView: The UITextView whose height is to be changed.
     /// - Returns: The duration of the animation, in seconds.
