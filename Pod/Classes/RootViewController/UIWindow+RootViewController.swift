8af7ee1102b7ac69db1bb28832fd7bd97a020c6c
Michael Skiba <mike.skiba@raizlabs.com>
2016-09-08 16:02:53 -0400

    initial support for swift 3 in UIWindow+RootViewController

--- Pod/Classes/RootViewController/UIWindow+RootViewController.swift
+++ Pod/Classes/RootViewController/UIWindow+RootViewController.swift
@@ -8,25 +8,6 @@
 // Copyright 2016 Raizlabs and other contributors
 // http://raizlabs.com/
 //
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-//
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
 
 import UIKit
 
@@ -35,10 +16,35 @@ import UIKit
  */
 public extension UIWindow {
 
-
+    #if swift(>=3.0)
     /**
      Set the rootViewController on this UIWindow instance.
      
+     - parameter viewController: The view controller to set
+     - parameter animated:       Whether or not to animate the transition, animation is a cross-fade
+     - parameter completion:     Completion block to be invoked after the transition finishes
+     */
+    func setRootViewController(_ viewController: UIViewController, animated: Bool, completion: @escaping () -> Void = {}) {
+        if animated {
+
+            UIView.transition(with: self, duration: 0.3, options: .transitionCrossDissolve, animations: { () -> Void in
+                let oldState = UIView.areAnimationsEnabled
+                UIView.setAnimationsEnabled(false)
+
+                self.rootViewController = viewController
+
+                UIView.setAnimationsEnabled(oldState)
+            }, completion: { (finished) -> Void in
+                completion()
+            })
+        } else {
+            self.rootViewController = viewController
+        }
+    }
+    #else
+    /**
+     Set the rootViewController on this UIWindow instance.
+
      - parameter viewController: The view controller to set
      - parameter animated:       Whether or not to animate the transition, animation is a cross-fade
      - parameter completion:     Completion block to be invoked after the transition finishes
@@ -53,11 +59,12 @@ public extension UIWindow {
                 self.rootViewController = viewController
 
                 UIView.setAnimationsEnabled(oldState)
-            }, completion: { (finished) -> Void in
-                completion()
+                }, completion: { (finished) -> Void in
+                    completion()
             })
         } else {
             self.rootViewController = viewController
         }
     }
+    #endif
 }
