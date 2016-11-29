b1f3068ce4d8400de973588947177062d1055735
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2016-11-29 11:52:03 -0500

    Add clamping tests.

--- Example/Tests/MathTests.swift
+++ Example/Tests/MathTests.swift
@@ -3,10 +3,9 @@ import Swiftilities
 
 class Tests: XCTestCase {
 
+    let epsilon = 0.00001
+
     func testFloatingPointScale() {
-
-        let epsilon = 0.00001
-
         XCTAssertEqualWithAccuracy(1.0.scaled(from: 0.0...1.0, to: 0.0...1.0), 1.0, accuracy: epsilon)
         XCTAssertEqualWithAccuracy(0.5.scaled(from: 0.0...1.0, to: 0.0...100.0), 50.0, accuracy: epsilon)
 
@@ -24,6 +23,16 @@ class Tests: XCTestCase {
         // Negating remap(10)
         XCTAssertEqualWithAccuracy(-10.0.scaled(from: -50.0...0.0, to: 0.0...1.0), -1.2, accuracy: epsilon)
         XCTAssertEqualWithAccuracy(-10.0.scaled(from: -50.0...0.0, to: 0.0...1.0, clamp: true), -1.0, accuracy: epsilon)
+    }
+
+    func testClamping() {
+        XCTAssertEqual(0.clamped(to: -10...20), 0)
+        XCTAssertEqual((-10).clamped(to: -10...20), -10)
+        XCTAssertEqual(UInt(0).clamped(to: 10...20), 10)
+        XCTAssertEqualWithAccuracy(Double.pi.clamped(to: 0...1), 1, accuracy: epsilon)
+        XCTAssertEqualWithAccuracy(Double.pi.clamped(to: 3...4), Double.pi, accuracy: epsilon)
+        XCTAssertEqualWithAccuracy(-20.clamped(to: 0...1), -1, accuracy: epsilon)
+        XCTAssertEqualWithAccuracy((-20).clamped(to: 0...1), 0, accuracy: epsilon)
     }
 
 }
