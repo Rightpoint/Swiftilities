4bf90c69f7d82193bc3cf74a7fc6c9d853648b24
Michael Skiba <mike.skiba@raizlabs.com>
2017-02-23 11:43:54 -0500

    Sorted project files, added empty view controllers for example app

---
+++ Example/Swiftilities/Views/Lifecycle/LifecycleConsoleViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LifecycleConsoleViewController.swift
+//  Swiftilities
+//
+//  Created by Michael Skiba on 2/23/17.
+//  Copyright © 2017 Raizlabs. All rights reserved.
+//
+
+import UIKit
+
+class LifecycleConsoleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ Example/Swiftilities/Views/Lifecycle/LifecycleDemoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LifecycleDemoViewController.swift
+//  Swiftilities
+//
+//  Created by Michael Skiba on 2/23/17.
+//  Copyright © 2017 Raizlabs. All rights reserved.
+//
+
+import UIKit
+
+class LifecycleDemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
