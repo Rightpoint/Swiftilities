a9a9de78b1b002e0f4e9bdd61b1a292e3ee1447e
Michael Skiba <mike.skiba@raizlabs.com>
2017-02-23 12:24:20 -0500

    added example to project

--- Example/Swiftilities/Views/Lifecycle/LifecycleConsoleViewController.swift
+++ Example/Swiftilities/Views/Lifecycle/LifecycleConsoleViewController.swift
@@ -8,10 +8,34 @@
 
 import UIKit
 
+protocol LifecycleLogDelegate: class {
+
+    func logEvent(description: String)
+
+}
+
 class LifecycleConsoleViewController: UIViewController {
+
+    @IBOutlet weak var lifecycleLogTextView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func presentLifecycleController(_ sender: Any) {
+        let demoViewController = LifecycleDemoViewController()
+        demoViewController.lifecycleDelegate = self
+        let navigationViewController = UINavigationController(rootViewController: demoViewController)
+        present(navigationViewController, animated: true, completion: nil)
+    }
+
+}
+
+extension LifecycleConsoleViewController: LifecycleLogDelegate {
+
+    func logEvent(description: String) {
+        let currentText = lifecycleLogTextView.text ?? ""
+        lifecycleLogTextView.text = "\(currentText)\n\(description)"
     }
 
 }
--- Example/Swiftilities/Views/Lifecycle/LifecycleDemoViewController.swift
+++ Example/Swiftilities/Views/Lifecycle/LifecycleDemoViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class LifecycleDemoViewController: UIViewController {
 
+    weak var lifecycleDelegate: LifecycleLogDelegate?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        addBehaviors([LogViewEventsBehavior()])
+        view.backgroundColor = .white
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissSelf(sender:)))
+    }
+
+    func dismissSelf(sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
     }
 
 }
