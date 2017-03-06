e3892f7be2606820f956451f618f178657596154
Zev Eisenberg <igeek1@gmail.com>
2017-03-06 16:50:00 -0500

    Stop calling .init explicitly.

--- Example/Swiftilities/App Delegate/AppDelegate.swift
+++ Example/Swiftilities/App Delegate/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
-        DefaultBehaviors.init(behaviors: [LogAppearanceBehavior()]).inject()
+        DefaultBehaviors(behaviors: [LogAppearanceBehavior()]).inject()
         return true
     }
 }
