4e6680cc7ed3726ad9b7dc9434e7e4139d90139a
Nick Bonatsakis <nbonatsakis@gmail.com>
2017-07-10 16:26:37 -0400

    - Use newer sim

--- scripts/iOS.sh
+++ scripts/iOS.sh
@@ -5,7 +5,7 @@ set -o pipefail && \
   -workspace ./Example/Swiftilities.xcworkspace \
   -scheme Swiftilities \
   -sdk iphonesimulator \
-  -destination "platform=iOS Simulator,name=iPhone 7,OS=10.0" \
+  -destination "platform=iOS Simulator,name=iPhone 7,OS=10.1" \
   CODE_SIGNING_REQUIRED=NO \
   CODE_SIGN_IDENTITY= \
   | xcpretty
