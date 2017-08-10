420123ec25bb58192ad7228650c8717f81ceb4b5
Jay Clark <jason.clark@raizlabs.com>
2017-08-10 12:21:25 -0400

    some compiler warning cleanup

--- Pod/Classes/Math/FloatingPoint+Scale.swift
+++ Pod/Classes/Math/FloatingPoint+Scale.swift
@@ -18,7 +18,7 @@ public extension FloatingPoint {
     ///   - reversed: whether the output mapping should be revserd, such that
     ///               as the input increases, the output decreases. Defaults to `false`.
     /// - Returns: The input number, scaled from the `from` range to the `to` range.
-    public func scaled(from source: ClosedRange<Self>, to destination: ClosedRange<Self>, timingTransform: ((Self) -> Self)? = nil, clamped: Bool = false, reversed: Bool = false) -> Self {
+    public func scaled(from source: ClosedRange<Self>, to destination: ClosedRange<Self>, clamped: Bool = false, reversed: Bool = false, transform: ((Self) -> Self)? = nil) -> Self {
 
         let destinationStart = reversed ? destination.upperBound : destination.lowerBound
         let destinationEnd = reversed ? destination.lowerBound : destination.upperBound
@@ -29,8 +29,8 @@ public extension FloatingPoint {
         let destinationUpperMinusLower = destinationEnd - destinationStart
 
         let percentThroughSource = (selfMinusLower / sourceUpperMinusLower)
-        let curvedPercent = timingTransform?(percentThroughSource) ?? percentThroughSource
-        var result =  curvedPercent * destinationUpperMinusLower + destinationStart
+        let curvedPercent = transform?(percentThroughSource) ?? percentThroughSource
+        var result = curvedPercent * destinationUpperMinusLower + destinationStart
 
         if clamped {
             result = result.clamped(to: destination)
@@ -50,23 +50,19 @@ public extension BinaryFloatingPoint {
     }
 
     public func transformed(from source: ClosedRange<Self>, to destination: ClosedRange<Self>, timingCurve: UICubicTimingParameters = UICubicTimingParameters(animationCurve: .linear), clamped: Bool = false, reversed: Bool = false) -> Self {
-        let transform: ((Self) -> Self)? = {
-            guard timingCurve.animationCurve != .linear else { return nil }
-            return { t -> Self in
-                return Self(CubicBezier.value(for: CGFloat(t.doubleValue),
-                                              controlPoint1: timingCurve.controlPoint1,
-                                              controlPoint2: timingCurve.controlPoint2).doubleValue)
-            }
-        }()
 
-        return self.scaled(from: source, to: destination, timingTransform: transform, clamped: clamped, reversed: reversed)
+        return self.scaled(from: source, to: destination, clamped: clamped, reversed: reversed) { input -> Self in
+            guard timingCurve.animationCurve != .linear else { return input }
+            return Self(CubicBezier.value(for: CGFloat(input.doubleValue),
+                                          controlPoint1: timingCurve.controlPoint1,
+                                          controlPoint2: timingCurve.controlPoint2).doubleValue)
+        }
     }
 
     private var doubleValue: Double {
         switch self {
         case let value as Double: return value
         case let value as Float: return Double(value)
-        case let value as Float80: return Double(value)
         case let value as CGFloat: return Double(value)
         default: fatalError("Unsupported floating point type")
         }
