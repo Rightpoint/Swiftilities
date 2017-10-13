1b270379deba99fc1f5b1440fa0df0f0fb6626b2
Nicholas Bonatsakis <nbonatsakis@gmail.com>
2017-10-12 22:06:40 -0400

    - Repalce public with open for better button

--- Pod/Classes/BetterButton/BetterButton.swift
+++ Pod/Classes/BetterButton/BetterButton.swift
@@ -21,7 +21,7 @@ private struct StyleConstants {
 }
 
 /// A "better" version of `UIButton` that supports various button styles and shapes.
-public open class BetterButton: UIButton {
+open class BetterButton: UIButton {
 
     /// The shape of the button.
     ///
