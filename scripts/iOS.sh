c16df8369536901dd9aface993902a51c5a2efe1
Zev Eisenberg <zev.eisenberg@raizlabs.com>
2018-03-02 10:42:46 -0500

    Build on iOS 10.3.1.

--- scripts/iOS.sh
+++ scripts/iOS.sh
@@ -5,7 +5,7 @@ set -o pipefail && \
   -workspace ./Example/Swiftilities.xcworkspace \
   -scheme Swiftilities \
   -sdk iphonesimulator \
-  -destination "platform=iOS Simulator,name=iPhone 7,OS=10.1" \
+  -destination "platform=iOS Simulator,name=iPhone 7,OS=10.3.1" \
   CODE_SIGNING_REQUIRED=NO \
   CODE_SIGN_IDENTITY= \
   | xcpretty
