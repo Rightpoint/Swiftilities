aadfe17291bc741f76f2811030c2fccf07bffd5b
Chris Ballinger <chris.ballinger@raizlabs.com>
2018-06-18 15:50:20 -0700

    open readonly

--- Pod/Classes/AboutView/AboutView.swift
+++ Pod/Classes/AboutView/AboutView.swift
@@ -10,7 +10,7 @@ import UIKit
 
 open class AboutView: UIView {
 
-    public let imageView: UIImageView = {
+    open private(set) var imageView: UIImageView = {
         let imageView = UIImageView()
 
         imageView.accessibilityTraits = UIAccessibilityTraitNone
@@ -22,7 +22,7 @@ open class AboutView: UIView {
         return imageView
     }()
 
-    public let label: UILabel = {
+    open private(set) var label: UILabel = {
         let label = UILabel()
 
         label.backgroundColor = .clear
