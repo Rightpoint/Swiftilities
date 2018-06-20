a4d51b7dcf76e9db77730341142054d1605f40a9
Chris Ballinger <chris.ballinger@raizlabs.com>
2018-06-20 11:31:29 -0700

    Use Collection

--- Pod/Classes/Compatibility/Compatibility.swift
+++ Pod/Classes/Compatibility/Compatibility.swift
@@ -10,9 +10,9 @@ import Foundation
 
 #if swift(>=4.1)
 #else
-    extension Array {
-        func compactMap<ElementOfResult>(_ transform: (Element) throws -> ElementOfResult?) rethrows -> [ElementOfResult] {
-            return try flatMap(transform)
-        }
+extension Collection {
+    func compactMap<ElementOfResult>(_ transform: (Element) throws -> ElementOfResult?) rethrows -> [ElementOfResult] {
+        return try flatMap(transform)
     }
+}
 #endif
